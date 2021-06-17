81110030a5231ad742d97184d4c1ded0924b7f18
Timur Akopyan <akopyan.t.i@inbox.ru>
2021-06-17 16:32:06 +0300

    Huffman encoding. Completed

--- Computer Science Center. Methods/4.2/s05_Huffman_encoding.py
+++ Computer Science Center. Methods/4.2/s05_Huffman_encoding.py
@@ -5,9 +5,14 @@
 # В следующих k строках запишите коды букв в формате "char: code".
 # В последней строке выведите закодированную строку.
 
-class Node:
-    def __init__(self, _weight, _left, _right):
+class Weighted:
+    def __init__(self, _weight):
         self.weight = _weight
+
+
+class Node(Weighted):
+    def __init__(self, _weight, _left, _right):
+        Weighted.__init__(self, _weight)
         self.left = _left
         self.right = _right
 
@@ -15,10 +20,10 @@ class Node:
         return 'Node {weight: ' + str(self.weight) + ', left: ' + str(self.left) + ', right: ' + str(self.right) + '}'
 
 
-class Leaf:
+class Leaf(Weighted):
     def __init__(self, _name, _weight):
         self.name = _name
-        self.weight = _weight
+        Weighted.__init__(self, _weight)
 
     def __str__(self):
         return 'Leaf {name: ' + str(self.name) + ', weight: ' + str(self.weight) + '}'
@@ -49,16 +54,45 @@ class Forrest:
         return result
 
 
+def code_former(_codes, _unit, _current_code):
+    """
+    :param dict _codes:
+    :param Weighted _unit:
+    :param str _current_code:
+    """
+    if isinstance(_unit, Leaf):
+        _codes[_unit.name] = _current_code
+    elif isinstance(_unit, Node):
+        code_former(_codes, _unit.left, _current_code + '0')
+        code_former(_codes, _unit.right, _current_code + '1')
+    else:
+        print('Unknown type of unit')
+
+
+def tree_to_codes(_tree, _codes):
+    """
+    :param Weighted _tree:
+    :param dict _codes:
+    """
+    if isinstance(_tree, Leaf):
+        _codes[_tree.name] = '0'
+    elif isinstance(_tree, Node):
+        code_former(_codes, _tree, '')
+    else:
+        print('Unknown type of unit')
+
+
 string = input()
 dictionary = {}
+forrest = Forrest()
+codes = {}
+encoded_string = ''
 
 for char in string:
     if char in dictionary:
         dictionary[char] += 1
     else:
         dictionary[char] = 1
-
-forrest = Forrest()
 
 for letter, weight in dictionary.items():
     forrest.insert(Leaf(letter, weight))
@@ -68,4 +102,12 @@ while len(forrest.content) > 1:
     j = forrest.extract_minimum()
     forrest.insert(Node(i.weight + j.weight, i, j))
 
-print(forrest)
+tree_to_codes(forrest.content[0], codes)
+
+for char in string:
+    encoded_string += codes[char]
+
+print(len(codes), len(encoded_string))
+for letter, code in codes.items():
+    print(letter + ':', code)
+print(encoded_string)
